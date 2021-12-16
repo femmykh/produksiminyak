49b6c9dc9f7a8e5649bb9ec128aa44090940ea5e
femmykh <96212266+femmykh@users.noreply.github.com>
2021-12-16 09:53:09 +0700

    Update produksiminyak.py

--- produksiminyak.py
+++ produksiminyak.py
@@ -4,7 +4,10 @@ import streamlit as st
 
 # Main Page
 st.set_page_config(page_title = "Pusat Informasi Negara Penghasil Minyak", page_icon = "penguin", layout = "wide")
-st.header("Pusat Informasi Negara Penghasil Minyak")
+col1, col2 = st.columns((0.05, 1))
+col1.image("logo_itb_1024.png", width=100)
+title = '<p style="font-family: sans-serif; font-size: 40px; text-align: center;"><b>Pusat Informasi Negara Penghasil Minyak</b></p>'
+col2.markdown(title, unsafe_allow_html=True)
 # Creator
 st.markdown("**Femmy Khairany Hemas** — **12220110**")
 
